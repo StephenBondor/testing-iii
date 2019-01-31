e151e6054591c2b9a3f50a7206a05b61ca9d6c07
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2019-01-30 16:01:07 -0800

    Add basic tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
 // Test away
+import React from 'react';
+import {render, fireEvent} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Dashboard from '../dashboard/Dashboard';
+
+describe('<Dashboard /', () => {
+	it('shows the display', () => {
+		const {getByTestId} = render(<Dashboard />);
+		expect(getByTestId('display')).toBeInTheDocument();
+	});
+	it('shows the controls', () => {
+		const {getByTestId} = render(<Dashboard />);
+		expect(getByTestId('controls')).toBeInTheDocument();
+	});
+});
--- src/display/Display.js
+++ src/display/Display.js
@@ -1,20 +1,24 @@
 import React from 'react';
 
-const Display = ({ closed, locked }) => {
-  const closedClass = `led ${closed ? 'red-led' : 'green-led'}`;
-  const lockedClass = `led ${locked ? 'red-led' : 'green-led'}`;
+const Display = ({closed, locked}) => {
+	const closedClass = `led ${closed ? 'red-led' : 'green-led'}`;
+	const lockedClass = `led ${locked ? 'red-led' : 'green-led'}`;
 
-  return (
-    <div className="display panel">
-      <div className={lockedClass}>{locked ? 'Locked' : 'Unlocked'}</div>
-      <div className={closedClass}>{closed ? 'Closed' : 'Open'}</div>
-    </div>
-  );
+	return (
+		<div className='display panel' data-testid='display'>
+			<div className={lockedClass} data-testid='locked'>
+				{locked ? 'Locked' : 'Unlocked'}
+			</div>
+			<div className={closedClass} data-testid='closed'>
+				{closed ? 'Closed' : 'Open'}
+			</div>
+		</div>
+	);
 };
 
 Display.defaultProps = {
-  closed: false,
-  locked: false,
+	closed: false,
+	locked: false
 };
 
 export default Display;
